5f19a512ede5ddb3df25beeb7715e7494c67687f
Derek Graham <deejaygraham@gmail.com>
2014-11-26 09:43:23 +0000

    Added xunit tests

--- NumberSpeller.Tests/NumberSpellerTests.cs
+++ NumberSpeller.Tests/NumberSpellerTests.cs
@@ -1,35 +1,85 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace NumberSpeller.Tests
 {
-	[TestClass]
 	public class NumberSpellerTests
 	{
-		[TestMethod]
+		[Fact]
 		public void Spelling_Nothing_Returns_Zero()
 		{
-			long zero = 0;
-
-			Assert.AreEqual("zero", zero.Spell());
+            AssertSpelling(0, "zero");
 		}
 
-		[TestMethod]
+        [Fact]
 		public void Spelling_1_Returns_One()
 		{
-			long one = 1;
-
-			Assert.AreEqual("one", one.Spell());
+            AssertSpelling(1, "one");
 		}
 
-		[TestMethod]
+        [Fact]
 		public void Spelling_2_Returns_Two()
 		{
-			long two = 2;
-
-			Assert.AreEqual("two", two.Spell());
+            AssertSpelling(2, "two");
 		}
 
-	}
+        [Fact]
+		public void Spelling_10_Returns_Ten()
+		{
+            AssertSpelling(10, "ten");
+		}
 
+        [Fact]
+		public void Spelling_15_Returns_Fifteen()
+		{
+            AssertSpelling(15, "fifteen");
+		}
+
+        [Fact]
+        public void Spelling_21_Returns_Twenty_Space_One()
+        {
+            AssertSpelling(21, "twenty one");
+        }
+
+        [Fact]
+        public void Spelling_99_Returns_Ninety_Space_Nine()
+        {
+            AssertSpelling(99, "ninety nine");
+        }
+
+        [Fact]
+        public void Spelling_300_Returns_Three_Space_Hundred()
+        {
+            AssertSpelling(300, "three hundred");
+        }
+
+        [Fact]
+        public void Spelling_310_Returns_Three_Space_Hundred_Space_And_Space_Ten()
+        {
+            AssertSpelling(310, "three hundred and ten");
+        }
+
+        [Fact]
+        public void Spelling_1501_Returns_OneThousand_Five_Hundred_And_One()
+        {
+            AssertSpelling(1501, "one thousand five hundred and one");
+        }
+
+        [Fact]
+        public void Spelling_12609_Returns_Twelve_Thousand_Six_Hundred_And_Nine()
+        {
+            AssertSpelling(12609, "twelve thousand six hundred and nine");
+        }
+
+        [Fact]
+        public void Spelling_512607_Returns_Five_Hundred_And_Twelve_Thousand_Six_Hundred_And_Seven()
+        {
+            AssertSpelling(512607, "five hundred and twelve thousand six hundred and seven");
+        }
+
+        private void AssertSpelling(long value, string text)
+        {
+            Assert.Equal(text, value.Spell());
+        }
+	}
 }
--- NumberSpeller/NumberSpeller.cs
+++ NumberSpeller/NumberSpeller.cs
@@ -1,30 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberSpeller
 {
     public static class NumberSpeller
     {
+		private static Dictionary<long, string> understoodQuantities = new Dictionary<long, string>();
+
+		static NumberSpeller()
+		{
+			understoodQuantities.Add( 0, "zero");
+			understoodQuantities.Add( 1, "one");
+			understoodQuantities.Add( 2, "two");
+			understoodQuantities.Add( 3, "three");
+			understoodQuantities.Add( 4, "four");
+			understoodQuantities.Add( 5, "five");
+			understoodQuantities.Add( 6, "six");
+			understoodQuantities.Add( 7, "seven");
+			understoodQuantities.Add( 8, "eight");
+			understoodQuantities.Add( 9, "nine");
+			understoodQuantities.Add(10, "ten");
+			understoodQuantities.Add(11, "eleven");
+			understoodQuantities.Add(12, "twelve");
+			understoodQuantities.Add(13, "thirteen");
+			understoodQuantities.Add(14, "fourteen");
+			understoodQuantities.Add(15, "fifteen");
+			understoodQuantities.Add(16, "sixteen");
+			understoodQuantities.Add(17, "seventeen");
+			understoodQuantities.Add(18, "eighteen");
+			understoodQuantities.Add(19, "nineteen");
+			understoodQuantities.Add(20, "twenty");
+			understoodQuantities.Add(30, "thirty");
+			understoodQuantities.Add(40, "forty");
+			understoodQuantities.Add(50, "fifty");
+			understoodQuantities.Add(60, "sixty");
+			understoodQuantities.Add(70, "seventy");
+			understoodQuantities.Add(80, "eighty");
+			understoodQuantities.Add(90, "ninety");
+		}
+
 		public static string Spell(this long value)
 		{
-			switch (value)
-			{
-				case 0: return "zero";
-				case 1: return "one";
-				case 2: return "two";
-				case 3: return "three";
-				case 4: return "four";
-				case 5: return "five";
-				case 6: return "six";
-				case 7: return "seven";
-				case 8: return "eight";
-				case 9: return "nine";
-			}
+            if (understoodQuantities.ContainsKey(value))
+                return understoodQuantities[value];
 
-			throw new ArgumentException(string.Format("Can't spell \'{0}\'", value));
+            const long OneThousand = 1000;
+
+            if (value >= OneThousand)
+            {
+                long quotient = value / OneThousand;
+                long remainder = value % OneThousand;
+
+                string thousands = Spell(quotient);
+                string rest = string.Empty;
+
+                if (remainder > 0)
+                    rest = " " + Spell(remainder);
+
+                return thousands + " thousand" + rest;
+            }
+
+            const long OneHundred = 100;
+
+            if (value >= OneHundred)
+            {
+                long quotient = value / OneHundred;
+                long remainder = value % OneHundred;
+
+                string hundreds = Spell(quotient);
+                string rest = string.Empty;
+
+                if (remainder > 0)
+                    rest = " and "+ Spell(remainder);
+
+                return hundreds + " hundred" + rest;
+            }
+
+            const long Twenty = 20;
+
+            if (value > Twenty)
+            {
+                long units = value % 10;
+                long tens = value - units;
+
+                return Spell(tens) + " " + Spell(units);
+            }
+
+            throw new ArgumentOutOfRangeException("Can't spell \'" + value.ToString() + "\'");
 		}
     }
 }
